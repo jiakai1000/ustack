7fc38e492e1a8add28d3ac4b1810f4ba9e51fe6d
jiakai1000 <jiakai1000@gmail.com>
2016-04-15 19:01:38 +0800

    d

--- tcp.c
+++ tcp.c
@@ -1,2 +1,19 @@
 #include "tcp.h"
+#include <rte_thash.h>
 
+int 
+tcp_connect(tcp_t *tcp, const struct sockaddr *addr, socklen_t addrlen, tcp_connect_cb cb)
+{
+    struct sockaddr_in *inaddr;
+
+    switch (addr->sa_family) {
+    case AF_INET:
+        inaddr = (struct sockaddr_in *)addr;
+        //inaddr->sin_addr.s_addr
+        break;
+    case AF_INET6:
+    default:
+        return -EAFNOSUPPORT;
+    }
+    //rte_softrss_be();
+}
